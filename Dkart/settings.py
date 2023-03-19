6e66963790c5b9e1801524106ca16340f5182a3f
vamsivallepu <vallepu670@gmail.com>
2023-03-19 11:34:32 +0530

    added custom user model

--- Dkart/settings.py
+++ Dkart/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'category',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Dkart.wsgi.application'
+AUTH_USER_MODEL = 'accounts.Account'
 
 
 # Database
@@ -121,6 +123,9 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'Dkart/static',
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- category/models.py
+++ category/models.py
@@ -5,9 +5,13 @@ from django.db import models
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
-    slug = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(max_length=255, blank=True)
     category_img = models.ImageField(upload_to='photos/categories/', blank=True)
 
     def __str__(self):
         return self.category_name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
